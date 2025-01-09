865f34200a9e9bd0181554810833c05a1b88c2e5
cindy <dlruddus76@gmail.com>
2025-01-09 22:48:00 +0900

    디저트 js 추가

---
+++ src/assets/dessert.js
@@ -0,0 +1,50 @@
+export default [
+    {
+        id: 0,
+        title: '한입 초코 츄러스',
+        image: 'https://www.mcdonalds.co.kr/upload/product/pcfile/1736299008239.png',
+        content: '바삭한 초코 츄러스에 가득 찬 진한 초코 필링!\n함께 제공되는 시나몬 슈가를 뿌려서 더욱 달콤하게 즐겨보세요.',
+        price: '3,200',
+        priceNum: 3200,
+        quantity: 1
+    },
+    {
+        id: 1,
+        title: '베리 스트로베리 맥플러리',
+        image: 'https://www.mcdonalds.co.kr/upload/product/pcfile/1617837982953.png',
+        content:
+            '신선한 1등급 원유에 국내산 딸기 과즙이 들어간 스트로베리 아이스크림에 오레오가 블랜드 되어 있는 맥플러리',
+        price: '4,300',
+        priceNum: 4300,
+        quantity: 1
+    },
+    {
+        id: 2,
+        title: '오레오 맥플러리',
+        image: 'https://www.mcdonalds.co.kr/upload/product/pcfile/1615378578396.png',
+        content:
+            '우유 듬뿍 신선한 아이스크림에 아삭아삭 오레오 쿠키가 가득!',
+        price: '4,300',
+        priceNum: 4300,
+        quantity: 1
+    },
+    {
+        id: 3,
+        title: '바닐라 쉐이크',
+        image: 'https://www.mcdonalds.co.kr/uploadFolder/product/prov_201901290255488970.png',
+        content: '신선한 우유에 달콤한 바닐라 시럽으로 맛을 더한 깔끔한 쉐이크!',
+        price: '3,500',
+        priceNum: 3500,
+        quantity: 1
+    },
+    {
+        id: 4,
+        title: '초코 쉐이크',
+        image: 'https://www.mcdonalds.co.kr/uploadFolder/product/prov_201901290257444650.png',
+        content:
+            '신선한 우유에 달콤함 초코렛 시럽이 들어간 부드러운 쉐이크!',
+        price: '3,500',
+        priceNum: 3500,
+        quantity: 1
+    }
+];
