50c1b620b9f64668355bea3b4e2a2c1a140c8163
cindy <dlruddus76@gmail.com>
2025-01-09 22:50:52 +0900

    스낵앤사이드 js 추가

---
+++ src/assets/snackside.js
@@ -0,0 +1,50 @@
+export default [
+    {
+        id: 0,
+        title: '드립 커피',
+        image: 'https://www.mcdonalds.co.kr/upload/product/pcfile/1732002751957.png',
+        content:
+            '갓 내린 부드러운 맛!\n드립 커피의 부드러움을 만나보세요.',
+        price: '2,700',
+        priceNum: 2700,
+        quantity: 1
+    },
+    {
+        id: 1,
+        title: '한입 초코 츄러스',
+        image: 'https://www.mcdonalds.co.kr/upload/product/pcfile/1736299008239.png',
+        content: '바삭한 초코 츄러스에 가득 찬 진한 초코 필링!\n함께 제공되는 시나몬 슈가를 뿌려서 더욱 달콤하게 즐겨보세요.',
+        price: '3,200',
+        priceNum: 3200,
+        quantity: 1
+    },
+    {
+        id: 2,
+        title: '컬리 후라이',
+        image: 'https://www.mcdonalds.co.kr/upload/product/pcfile/1735004609750.png',
+        content:
+            '돌돌 말려있는 형태가 특징인 통 감자 후라이\n*판매 시간: 오전 10시 30분 ~ 새벽 4시',
+        price: '3,500',
+        priceNum: 3500,
+        quantity: 1
+    },
+    {
+        id: 3,
+        title: '슈림프 스낵랩',
+        image: 'https://www.mcdonalds.co.kr/upload/product/pcfile/1736300408618.png',
+        content: '통통한 새우살 가득한 슈림프 패티에\n화이트마요와 스위트 칠리 소스로 매콤달콤하게!',
+        price: '3,300',
+        priceNum: 3300,
+        quantity: 1
+    },
+    {
+        id: 4,
+        title: '맥너겟®',
+        image: 'https://www.mcdonalds.co.kr/upload/product/pcfile/1612402131024.png',
+        content:
+            '바삭하고 촉촉한 치킨이 한 입에 쏙!\n다양한 소스로 입맛에 맞게 즐겨보세요!',
+        price: '3,400',
+        priceNum: 3400,
+        quantity: 1
+    }
+];
