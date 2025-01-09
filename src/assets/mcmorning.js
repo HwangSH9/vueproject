e27d72de9d74c5f0bbd551dbf3a04cd19d9af883
cindy <dlruddus76@gmail.com>
2025-01-09 22:49:55 +0900

    맥모닝 js 추가

---
+++ src/assets/mcmorning.js
@@ -0,0 +1,50 @@
+export default [
+    {
+        id: 0,
+        title: '베이컨 에그 맥그리들',
+        image: 'https://www.mcdonalds.co.kr/upload/product/pcfile/1680763652297.png',
+        content:
+            '메이플 시럽이 들어간 달콤한 핫케이크 번에\n국내산 1+등급 계란, 바삭한 베이컨으로 완성한 단짠의 맛!\n맥그리들과 함께 색다른 아침을 즐겨 보세요!',
+        price: '5,200',
+        priceNum: 5200,
+        quantity: 1
+    },
+    {
+        id: 1,
+        title: '소시지 에그 맥그리들',
+        image: 'https://www.mcdonalds.co.kr/upload/product/pcfile/1680763520726.png',
+        content: '메이플 시럽이 들어간 달콤한 핫케이크 번에\n국내산 1+등급 계란, 짭짤한 소시지 패티로 완성한 단짠의 맛!',
+        price: '5,500',
+        priceNum: 5500,
+        quantity: 1
+    },
+    {
+        id: 2,
+        title: '베이컨 토마토 에그 머핀',
+        image: 'https://www.mcdonalds.co.kr/upload/product/pcfile/1646207407116.png',
+        content:
+            '갓 구워내 따뜻하고 신선한\n베이컨 토마토 에그 머핀\n*판매 시간: 새벽 4시 ~ 오전 10시 30분',
+        price: '4,500',
+        priceNum: 4500,
+        quantity: 1
+    },
+    {
+        id: 3,
+        title: '치킨 치즈 머핀',
+        image: 'https://www.mcdonalds.co.kr/upload/product/pcfile/1597040023675.png',
+        content: '바삭한 치킨 패티와 고소한 치즈로 아침에도 든든하게!\n*판매 시간: 새벽 4시 ~ 오전 10시 30분',
+        price: '4,500',
+        priceNum: 4500,
+        quantity: 1
+    },
+    {
+        id: 4,
+        title: '디럭스 브렉퍼스트',
+        image: 'https://www.mcdonalds.co.kr/uploadFolder/product/prov_201901290309431230.png',
+        content:
+            '정통 아메리칸 스타일의 브렉퍼스트와 달콤한 핫케익을 함께~\n프리미엄한 주말 브런치를 매일 즐겨보세요.\n*판매 시간: 새벽 4시 ~ 오전 10시 30분',
+        price: '6,000',
+        priceNum: 6000,
+        quantity: 1
+    }
+];
