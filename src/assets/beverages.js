bc6726dc3732ad2a440b9dcff2b9e1bb0578755d
cindy <dlruddus76@gmail.com>
2025-01-09 22:46:30 +0900

    음료 js 추가

---
+++ src/assets/beverages.js
@@ -0,0 +1,50 @@
+export default [
+    {
+        id: 0,
+        title: '골드맥피즈',
+        image: 'https://www.mcdonalds.co.kr/upload/product/pcfile/1735006305275.png',
+        content:
+            '달콤상큼한 리얼 오렌지 시럽과 청량한 스프라이트의 만남',
+        price: '3,100',
+        priceNum: 3100,
+        quantity: 1
+    },
+    {
+        id: 1,
+        title: '코카-콜라 제로',
+        image: 'https://www.mcdonalds.co.kr/upload/product/pcfile/1583890021601.png',
+        content: '상쾌한 맛은 살리면서 웰빙 트렌드에 맞춰\n설탕과 칼로리를 제로로 줄인 코카-콜라 제로',
+        price: '2,600',
+        priceNum: 2600,
+        quantity: 1
+    },
+    {
+        id: 2,
+        title: '바닐라 라떼',
+        image: 'https://www.mcdonalds.co.kr/upload/product/pcfile/1677678794782.png',
+        content:
+            '바로 내린 100% 친환경 커피의 진한 맛과 향,\n1A등급 우유, 그리고 천연 바닐라 향으로 달콤함까지!',
+        price: '4,500',
+        priceNum: 4500,
+        quantity: 1
+    },
+    {
+        id: 3,
+        title: '아이스 바닐라 라떼',
+        image: 'https://www.mcdonalds.co.kr/upload/product/pcfile/1654652269651.png',
+        content: '바로 내린 100% 친환경 커피의 진한 맛과 향,\n1A등급 우유, 그리고 천연 바닐라 향으로 달콤함까지!',
+        price: '4,500',
+        priceNum: 4500,
+        quantity: 1
+    },
+    {
+        id: 4,
+        title: '카페라떼',
+        image: 'https://www.mcdonalds.co.kr/upload/product/pcfile/1677680118201.png',
+        content:
+            '바로 내린 100% 친환경 커피가 신선한 우유를 만나\n더 신선하고 부드럽게!',
+        price: '4,000',
+        priceNum: 4000,
+        quantity: 1
+    }
+];
