39131e3436c845945cca5bdb398f076007f51719
cindy <dlruddus76@gmail.com>
2025-01-09 22:49:12 +0900

    해피밀 js 추가

---
+++ src/assets/happymeal.js
@@ -0,0 +1,40 @@
+export default [
+    {
+        id: 0,
+        title: '에그 맥머핀',
+        image: 'https://www.mcdonalds.co.kr/uploadFolder/product/prov_201901290308474630.png',
+        content: '캐나디안 스타일 베이컨과 계란의 클래식한 만남,맥도날드의 아침을 탄생하게 한 바로 그 메뉴.\n*판매 시간: 새벽 4시 ~ 오전 10시 30분',
+        price: '5,500',
+        priceNum: 5500,
+        quantity: 1
+    },
+    {
+        id: 1,
+        title: '베이컨 에그 맥머핀',
+        image: 'https://www.mcdonalds.co.kr/uploadFolder/product/prov_201901290309065310.png',
+        content:
+            '부드러운 계란, 바삭바삭한 베이컨과 고소한 치즈가 갓 구워진 따뜻한 맥머핀®\n*판매 시간: 새벽 4시 ~ 오전 10시 30분',
+        price: '5,500',
+        priceNum: 5500,
+        quantity: 1
+    },
+    {
+        id: 2,
+        title: '소시지 에그 맥머핀',
+        image: 'https://www.mcdonalds.co.kr/uploadFolder/product/prov_201903190244458360.png',
+        content:
+            '잉글리쉬 머핀 위에 먹음직스럽게 올려진 치즈와 계란, 그리고 촉촉한 소시지 패티의 맛있는 조화!\n*판매 시간: 새벽 4시 ~ 오전 10시 30분',
+        price: '5,500',
+        priceNum: 5500,
+        quantity: 1
+    },
+    {
+        id: 3,
+        title: '핫케익',
+        image: 'https://www.mcdonalds.co.kr/uploadFolder/product/prov_201902111059484930.png',
+        content: '부드럽게 구워낸 핫케이크에 향긋한 버터와 달콤한 시럽을 살며시 부어 오늘 하루도 기분 좋게 시작하세요.\n*판매 시간: 새벽 4시 ~ 오전 10시 30분',
+        price: '5,500',
+        priceNum: 5500,
+        quantity: 1
+    }
+];
